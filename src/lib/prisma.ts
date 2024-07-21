e289ea21d8501521c06e7d7a2a57559b266ae65d
TJ Horner <me@tjhorner.com>
2024-07-20 20:44:25 -0700

    add cache

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,31 @@
-import { PrismaClient } from "@prisma/client"
+import { Prisma, PrismaClient } from "@prisma/client"
+import { Redis } from "ioredis"
+import { createPrismaRedisCache } from "prisma-redis-middleware"
 
 const prisma = new PrismaClient()
+
+if (process.env.REDIS_URL) {
+  const redis = new Redis(process.env.REDIS_URL)
+
+  const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
+    models: [
+      { model: "Jurisdiction" },
+      { model: "CloseCallReport" },
+      { model: "IncidentFactors", cacheTime: 3600 }
+    ],
+    storage: {
+      type: "redis",
+      options: {
+        client: redis,
+        invalidation: {
+          referencesTTL: 3600
+        }
+      }
+    },
+    cacheTime: 300
+  })
+
+  prisma.$use(cacheMiddleware)
+}
+
 export default prisma
