039d991d8e0b2494d06f61c7ff850f0f38ad0dc6
TJ Horner <me@tjhorner.com>
2024-08-09 09:59:22 -0700

    add atom feed

---
+++ src/routes/atom/+server.ts
@@ -0,0 +1,49 @@
+import prisma from "$lib/prisma"
+import type { RequestHandler } from "@sveltejs/kit"
+import { Feed } from "feed"
+
+export const GET: RequestHandler = async ({ url }) => {
+  const latestReports = await prisma.closeCallReport.findMany({
+    take: 20,
+    orderBy: {
+      reportedAt: "desc"
+    },
+    select: {
+      id: true,
+      reportedAt: true,
+      occurredAt: true,
+      latitude: true,
+      longitude: true,
+      transportationMode: true,
+      incidentFactors: true,
+    }
+  })
+
+  const feed = new Feed({
+    id: `${url.origin}/atom`,
+    copyright: "Close Call",
+    title: "Close Call Reports",
+    updated: new Date(latestReports[0].reportedAt),
+  })
+
+  for (const report of latestReports) {
+    feed.addItem({
+      id: `${url.origin}/reports/${report.id}`,
+      title: `Close Call Report #${report.id}`,
+      link: `${url.origin}/reports/${report.id}`,
+      date: new Date(report.reportedAt),
+      content: `
+        <p>Occurred at: ${report.occurredAt}</p>
+        <p>Location: ${report.latitude}, ${report.longitude}</p>
+        <p>Transportation mode: ${report.transportationMode}</p>
+        <p>Incident factors: ${report.incidentFactors.map(f => f.shortDescription).join(",")}</p>
+      `
+    })
+  }
+
+  return new Response(feed.atom1(), {
+    headers: {
+      "Content-Type": "application/atom+xml"
+    }
+  })
+}
